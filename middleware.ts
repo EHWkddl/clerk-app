6a0d9fb2b3cd70f1636a0f60800277d29a1b6914
정효민 <hyominj1014@gmail.com>
2025-10-21 04:40:23 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,21 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api(.*)"
+]);
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) {
-    await auth.protect()
+    auth().protect();
   }
-})
+});
 
 export const config = {
-  matcher: ['/((?!_next|.*\\..*).*)', '/(api)(.*)'],
-}
+  matcher: [
+    "/((?!_next|.*\\..*).*)",
+    "/"
+  ],
+};
